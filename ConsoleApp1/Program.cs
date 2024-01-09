30d2edff9bcccbbc77840cdf46bfb7d3e7a44fcb
KiAcademia <kistudentmeta5@gmail.com>
2024-01-09 17:35:10 +0900

    feat : Add BTree Base

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,6 +9,17 @@
             Console.WriteLine("S");
    
             Console.WriteLine("D");
+
+            BTree<int> BST = new BTree<int>();
+            BST.Insert(1);
+            BST.Insert(5);
+            BST.Insert(3);
+            BST.Insert(4);
+            BST.Insert(7);
+            BST.Insert(9);
+            BST.Remove(7);
+            BTNode<int> d = BST.Search(5);
+            Console.Write(d);
         }
     }
 }
